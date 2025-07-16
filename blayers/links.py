606b7141aae6308442ed3de163d9fa89e0256484
George Berry <george.e.berry@gmail.com>
2025-07-15 23:22:28 -0400

    update

--- blayers/links.py
+++ blayers/links.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from typing import Any
 
 import jax
-import numpyro.distributions as dist
+import numpyro.distributions as dists
 from numpyro import sample
 
 
@@ -21,9 +21,9 @@ class Link(ABC):
 class LocScaleLink(Link):
     def __init__(
         self,
-        sigma_dist: dist.Distribution = dist.Exponential,
+        sigma_dist: dists.Distribution = dists.Exponential,
         sigma_kwargs: dict[str, float] = {"rate": 1.0},
-        obs_dist: dist.Distribution = dist.Normal,
+        obs_dist: dists.Distribution = dists.Normal,
         obs_kwargs: dict[str, float] = {},
         dependent_outputs: bool = False,
     ) -> None:
@@ -52,13 +52,13 @@ class LocScaleLink(Link):
 
 
 gaussian_link_exp = LocScaleLink()
-lognormal_link_exp = LocScaleLink(obs_dist=dist.LogNormal)
+lognormal_link_exp = LocScaleLink(obs_dist=dists.LogNormal)
 
 
 class SingleParamLink(Link):
     def __init__(
         self,
-        obs_dist: dist.Distribution = dist.Bernoulli,
+        obs_dist: dists.Distribution = dists.Bernoulli,
         dependent_outputs: bool = False,
     ) -> None:
         self.obs_dist = obs_dist
@@ -79,4 +79,25 @@ class SingleParamLink(Link):
 
 
 logit_link = SingleParamLink()
-poission_link = SingleParamLink(obs_dist=dist.Poisson)
+poission_link = SingleParamLink(obs_dist=dists.Poisson)
+
+
+def negative_binomial_link(
+    y_hat: jax.Array,
+    y: jax.Array | None = None,
+    dependent_outputs: bool = False,
+    rate: float = 1.0,
+) -> jax.Array:
+    sigma = sample("sigma", dists.Exponential(rate=rate))
+
+    if dependent_outputs:
+        dist = dists.NegativeBinomial2(
+            mean=y_hat, concentration=sigma
+        ).to_event(1)
+    dist = dists.NegativeBinomial2(mean=y_hat, concentration=sigma)
+
+    return sample(
+        "obs",
+        dist,
+        obs=y,
+    )
